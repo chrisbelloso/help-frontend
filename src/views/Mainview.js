146e9740ecf1a6cc1b69b7684117bc649a5fcef4
chrisbelloso <christianbelloso2002@gmail.com>
2021-11-23 16:34:09 -0500

    newcommit

--- src/views/Mainview.js
+++ src/views/Mainview.js
@@ -1,8 +1,11 @@
-const MainView = async () => {
+const MainView = () => {
 
     return (
         <div>
             <h1>MAINVIEW</h1>
+            <button>
+                <a href="tel:+17866038365">CALL</a>
+            </button>
         </div>
     )
 }
